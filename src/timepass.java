4c6833ae4d106961c0ea9e49bb57a3c3abb6f65c
Akash Neharkar <124196448+akash-neharkar@users.noreply.github.com>
2024-07-29 19:57:31 +0530

    recursion

--- src/timepass.java
+++ src/timepass.java
@@ -1,6 +1,6 @@
 import java.sql.SQLOutput;
 import java.util.Scanner;
-
+//Practice
 public class timepass {
     public static void main(String[] args) {
         System.out.println("Hello World!");
