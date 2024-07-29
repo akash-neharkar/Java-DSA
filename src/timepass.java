0e675a17b717be8e8f958651ed2424450c9dd415
Akash Neharkar <124196448+akash-neharkar@users.noreply.github.com>
2024-07-29 20:00:42 +0530

    recursion

--- src/timepass.java
+++ src/timepass.java
@@ -4,9 +4,5 @@ import java.util.Scanner;
 public class timepass {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-
-        for (int i = 1; i < 6; i++) {
-            System.out.print("*" * int i);
-        }
     }
 }
