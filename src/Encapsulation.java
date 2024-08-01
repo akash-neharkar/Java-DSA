a8b9ce747e3beb24fa6b8ff7bcf679cc37c9c375
Akash Neharkar <124196448+akash-neharkar@users.noreply.github.com>
2024-08-02 01:34:10 +0530

    recursion

--- src/Encapsulation.java
+++ src/Encapsulation.java
@@ -5,10 +5,13 @@ class Human{
     private String name = "Akash";
     private int age = 21;
 
-    public int getAge(){
-        return age;
+    public Human(){
+        System.out.println("I am in constructor!");
     }
 
+    public int getAge() {
+        return age;
+    }
     public String getName(){
         return name;
     }
@@ -18,6 +21,7 @@ public class Encapsulation {
 
     public static void main(String[] args) {
         Human obj = new Human();
+        Human obj1 = new Human();
 //        obj.name = "Akash";
 //        obj.age = 21;
         System.out.println(obj.getName() + " " + obj.getAge());
---
+++ src/Inheritance.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Inheritance {
+    public static void main(String[] args) {
+        VeryAdvCalc obj = new VeryAdvCalc();
+        int r1 = obj.add(2,3);
+        System.out.println(r1);
+        int r2 = obj.sub(5,4);
+        System.out.println(r2);
+        int r3 = obj.multi(3,3);
+        System.out.println(r3);
+        int r4 = obj.div(3,3);
+        System.out.println(r4);
+        double r5 = obj.power(3,3);
+        System.out.println(r5);
+    }
+}
