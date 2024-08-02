0e76c78a5127537bb4227f303cdcb82061235b0d
Akash Neharkar <124196448+akash-neharkar@users.noreply.github.com>
2024-08-02 21:00:52 +0530

    Overriding

---
+++ src/Overriding.java
@@ -0,0 +1,23 @@
+
+
+class Calculator{
+    public int add(int n1, int n2) {
+        return n1+n2;
+    }
+}
+
+class AdvanceCalculator{
+    public int add(int n1, int n2){
+        return n1+n2+1;
+    }
+}
+
+public class Overriding {
+    public static void main(String[] args) {
+        System.out.println("This is method Overriding!");
+
+        AdvanceCalculator obj = new AdvanceCalculator();
+        int r1 = obj.add(3,3);
+        System.out.println(r1);
+    }
+}
