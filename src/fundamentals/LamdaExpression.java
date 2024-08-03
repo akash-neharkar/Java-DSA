bfdbc09f79ea1d57cda44db2db7e3b603f2d6aba
Akash Neharkar <124196448+akash-neharkar@users.noreply.github.com>
2024-08-03 14:58:29 +0530

    Lambda

--- src/fundamentals/LamdaExpression.java
+++ src/fundamentals/LamdaExpression.java
@@ -4,12 +4,18 @@ interface Lamda{
     void ex();
 }
 
+interface Lamda2{
+    void ex2(int i);
+}
+
 
 public class LamdaExpression {
     public static void main(String[] args) {
         Lamda obj = () -> // Lamda Expression "->" used to replace a few words of syntax which are known by Java
             System.out.println("I am in show");  //Curly brackets and ; is removed bcoz its just a one line method
-
             obj.ex();
+
+        Lamda2 obj2 = i -> System.out.println("I am in Lamda2");
+        obj2.ex2(5); //Lamda expression with parameters
     }
 }
