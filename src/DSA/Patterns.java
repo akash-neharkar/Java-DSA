d333ed1f94b5af0777fec05ec337c37421ed71e6
Akash Neharkar <124196448+akash-neharkar@users.noreply.github.com>
2024-08-06 00:52:40 +0530

    Sorting

---
+++ src/DSA/Patterns.java
@@ -0,0 +1,56 @@
+    package DSA;
+    import java.util.Scanner;
+
+
+    public class Patterns {
+        public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+            System.out.println("Enter the value of n");
+            int n = in.nextInt();
+            System.out.println("Pattern 1: ");
+            pattern(n);
+            System.out.println("Pattern 2: ");
+            pattern1(n);
+            System.out.println("Pattern 3: ");
+            pattern2(n);
+            System.out.println("Pattern 4: ");
+            pattern3(n);
+        }
+
+        static void pattern(int n){
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= i; j++) {
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+        }
+
+        static void pattern1(int n){
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+        }
+
+        static void pattern2(int n){
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n-i+1; j++) {
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+        }
+
+        static void pattern3(int n){
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= i; j++) {
+                    System.out.print(j);
+                }
+                System.out.println();
+            }
+        }
+
+    }
