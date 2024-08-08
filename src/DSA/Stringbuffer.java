38de5a69007f54a9fd4dcf9068516972ed89a741
Akash Neharkar <124196448+akash-neharkar@users.noreply.github.com>
2024-08-08 23:19:58 +0530

    Big

---
+++ src/DSA/Stringbuffer.java
@@ -0,0 +1,9 @@
+package DSA;
+
+public class Stringbuffer {
+    public static void main(String[] args) {
+        StringBuffer sb =  new StringBuffer();
+        sb.append("Hello");
+        System.out.println(sb.toString());
+    }
+}
