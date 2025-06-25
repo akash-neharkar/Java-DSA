5247f82d31d2235c450188424ad6709837041cb2
Akash Neharkar <124196448+akash-neharkar@users.noreply.github.com>
2025-06-25 16:18:08 +0530

    2025

--- src/BasicCodes/backtosquareone.java
+++ src/BasicCodes/backtosquareone.java
@@ -9,5 +9,22 @@ public class backtosquareone {
         System.out.println("Hello World!");
         Scanner in = new Scanner(System.in);
 
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        System.out.println("Enter the number you want to find: ");
+        int n = in.nextInt();
+
+        int res = findnumber(arr, n);
+        System.out.println(res);
+
+    }
+
+    public static int findnumber(int[] arr, int n){
+
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] == n){
+                return i;
+            }
+        }
+        return -1;
     }
 }
